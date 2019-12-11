878b202b49da568d5a9371247f98f2979b9bb536
abhay97ps <abhay97ps@gmail.com>
2019-12-10 22:33:47 -0500

    running exp1

---
+++ exp1.s
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:p40:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=100000
+#SBATCH --job-name=aps647
+#SBATCH --mail-user=aps647@nyu.edu
+#SBATCH --output=slurm_%j.out
+
+
+. ~/.bashrc
+source activate aps647
+python src/main.py
